f11d74521415ef4ac1fcc08b4f977b817ca53dfc
Jeff Yeon <jeff@wbrandstudio.com>
2018-02-14 14:21:01 -0800

    added meta

--- intelliskye_new/resources/views/layouts/layout.blade.php
+++ intelliskye_new/resources/views/layouts/layout.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="OC's Leading Software Providers">
+        <meta name="keywords" content="Software, App, Apps, Website, Design, Orange County, OC, Southern California">
+        <meta name="author" content="Nicholas Skye Hoyt">
         <link rel="icon" href="{{ asset('images/imagehere') }}">
         <title>IntelliSkye</title>
         <script src="https://use.fontawesome.com/b36c39fbb0.js"></script>
